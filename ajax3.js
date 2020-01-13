bbc9ebdc1ae6fc9202febceab7baf93b4575b802
orlando-gomes <55079964+orlando-gomes@users.noreply.github.com>
2020-01-13 02:34:54 -0300

    Add files via upload

---
+++ ajax3.js
@@ -0,0 +1,31 @@
+/*Exatamente o mesmo exemplo do ajax2, utilizando a biblioteca axios
+existente em "https://github.com/axios/axios"*/ 
+function buscar() {
+    /*
+    function myPromise() {
+        return new Promise(function (allright, bad) {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        allright(JSON.parse(xhr.responseText));
+                    } else {
+                        bad('Falha na conexão!');
+                    }
+                }
+            }
+            xhr.open('GET', 'https://api.github.com/users/orlando-gomes', true);
+            xhr.send(null);
+        })
+    }
+    */
+
+    axios.get('https://api.github.com/users/orlando-gomes')
+    .then(function (objSucess) {
+        document.getElementById('show').innerHTML = objSucess.login;
+    })
+    .catch(function (msg) {
+        console.warn(msg);
+    })
+
+}
